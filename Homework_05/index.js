47d6223134c5967da8197e43ffee1afcaab68158
Christine Ghazaryan <qghazaryan1@gmail.com>
2024-09-11 10:28:11 +0400

    home5updates

--- Homework_05/index.js
+++ Homework_05/index.js
@@ -17,15 +17,12 @@ let contactLookupPointer = null
 function filterContact(executionContext) {
     let formContext = executionContext.getFormContext()
     let customer = formContext.getAttribute('new_fk_customer').getValue()
-    // console.log('customer', customer);
-
 
     if (contactLookupPointer != null) {
         formContext.getControl('new_fk_contact').removePreSearch(contactLookupPointer)
     }
-    // customer = customer[0]?.id.replace('{', '').replace("}", '')
-    contactLookupPointer = filterFunction.bind({ "customer": customer[0]?.id })
 
+    contactLookupPointer = filterFunction.bind({ "customer": customer[0]?.id })
     formContext.getControl('new_fk_contact').addPreSearch(contactLookupPointer)
 }
 
@@ -33,32 +30,51 @@ async function filterFunction(executionContext) {
     let formContext = executionContext.getFormContext()
     console.log(this.customer);
 
-
-    let fetchXmlPosition = `
-        <fetch version="1.0" mapping="logical" savedqueryid="43ab67d7-f840-4284-bfaf-cc1da35db8ef"
-            no-lock="false" distinct="true">
-            <entity name="new_my_positions">
-                <attribute name="new_my_positionsid" />
-                <filter type="and">
-                    <condition attribute="new_fk_account" operator="eq"
-                        value="${this.customer}" />
-                </filter>
-            </entity>
-        </fetch>
-    `
-    fetchXmlPosition = "?fetchXml=" + encodeURIComponent(fetchXmlPosition)
-    const assetPositions = await Xrm.WebApi.retrieveMultipleRecords('new_my_positions', fetchXmlPosition)
-    const positions = assetPositions?.entities
-    console.log(positions);
-    for (let i = 0; i < positions.length; i++) {
-        const el = positions[i];
-        // I got positionIds but can't use
-        let fetchXmlContacts = `
+    let fetchXml = `
+                <fetch version="1.0" mapping="logical" savedqueryid="00184a6f-fe84-4e77-9776-be05df9ccbec"
+                    no-lock="false" distinct="true">
+                    <entity name="new_my_contact">
+                        <attribute name="new_my_contactid" />
+                        <filter type="and">
+                            <condition attribute="new_my_contactid" operator="not-null" />
+                        </filter>
+                        <link-entity name="new_my_positions" alias="aa" link-type="inner" from="new_fk_contact"
+                            to="new_my_contactid">
+                            <filter type="and">
+                                <condition attribute="new_fk_account" operator="eq"
+                                    value="${this.customer}"  />
+                            </filter>
+                        </link-entity>
+                    </entity>
+                </fetch>
         
-        `
+            `
+    fetchXml = "?fetchXml=" + encodeURIComponent(fetchXml)
+    const assets = await Xrm.WebApi.retrieveMultipleRecords('new_my_contact', fetchXml)
+    const contacts = assets?.entities
 
+    let fetchXmlContacts = `<filter type="or">`
+    for (let i = 0; i < contacts.length; i++) {
+        const contactId = contacts[i]['new_my_contactid']
+        fetchXmlContacts += `<condition attribute="new_my_contactid" operator="eq" value="{${contactId}}" />`
     }
-    formContext.getControl('new_fk_contact').addCustomFilter(fetchXmlContactFilter, 'new_my_contact')
+    fetchXmlContacts += `</filter>`
+    console.log(fetchXmlContacts);
+
+
+    // -------- Option 2 ----------- ???
+    // let fetchXmlContacts = `<filter type="and">
+    //         <condition attribute="new_my_contactid" operator="in">`
+
+    // for (let i = 0; i < contacts.length; i++) {
+    //     const contactId = contacts[i]['new_my_contactid']
+    //     fetchXmlContacts += `<value  uitype="new_my_contact">{${contactId}}</value>`
+    // }
+    // fetchXmlContacts += `</condition>
+    //                 </filter>`
+    // console.log(fetchXmlContacts);
+
+    formContext.getControl('new_fk_contact').addCustomFilter(fetchXmlContacts, 'new_my_contact')
 
 }
 
@@ -68,32 +84,22 @@ async function filterFunction(executionContext) {
 // field based on the "Inventory" and to get only products where "type"="Product"
 
 let productLookupPointer = null
-function filterContact(executionContext) {
+function filterProducts(executionContext) {
     let formContext = executionContext.getFormContext()
     let inventory = formContext.getAttribute('new_fk_inventory').getValue()
-    // console.log('customer', customer);
-
 
     if (productLookupPointer != null) {
         formContext.getControl('new_fk_product').removePreSearch(productLookupPointer)
     }
-    productLookupPointer = filterFunction.bind({ "inventory": inventory[0]?.id })
-
+    productLookupPointer = filterFunctionForProducts.bind({ "inventory": inventory[0]?.id })
     formContext.getControl('new_fk_product').addPreSearch(productLookupPointer)
 }
 
-function filterFunction(executionContext) {
+function filterFunctionForProducts(executionContext) {
     let formContext = executionContext.getFormContext()
-    // also with link-entity
     let fetchXml = `
-                <fetch version="1.0" mapping="logical" savedqueryid="977564c7-43b4-4deb-a9f5-7e3c64b4a8d3"
-                    no-lock="false" distinct="true">
+                <fetch version="1.0" mapping="logical" distinct="true">
                     <entity name="new_product">
-                        <attribute name="statecode" />
-                        <attribute name="new_name" />
-                        <order attribute="new_name" descending="false" />
-                        <attribute name="new_type" />
-                        <attribute name="new_price_per_unit" />
                         <attribute name="new_productid" />
                         <filter type="and">
                             <condition attribute="new_type" operator="eq" value="100000000" />
@@ -102,21 +108,30 @@ function filterFunction(executionContext) {
                             to="new_productid">
                             <filter type="and">
                                 <condition attribute="new_fk_inventory" operator="eq"
-                                    value="{028885cd-f45f-ef11-bfe2-000d3a48afd3}" uiname="Inventory 2"
-                                    uitype="new_inventory" />
+                                    value="${this.inventory}"/>
                             </filter>
                         </link-entity>
                     </entity>
                 </fetch>
             `
+    fetchXml = "?fetchXml=" + encodeURIComponent(fetchXml)
+    Xrm.WebApi.retrieveMultipleRecords('new_product', fetchXml).then((result) => {
+        const products = result?.entities
 
-    formContext.getControl('new_fk_product').addCustomFilter(fetchXml, 'new_product')
+        let fetchXmlProducts = `<filter type="or">`
+        for (let i = 0; i < products.length; i++) {
+            const productId = products[i]['new_productid']
+            fetchXmlProducts += `<condition attribute="new_productid" operator="eq" value="{${productId}}"  />`
+        }
+        fetchXmlProducts += `</filter>`
+        formContext.getControl('new_fk_product').addCustomFilter(fetchXmlProducts, 'new_product')
+    })
 
 }
 
 
 
-// Task 4
-// In the work order service entity, filter "Service" lookup to get only those products
-// where "type"="service"
-// done
+// // Task 4
+// // In the work order service entity, filter "Service" lookup to get only those products
+// // where "type"="service"
+// // done
